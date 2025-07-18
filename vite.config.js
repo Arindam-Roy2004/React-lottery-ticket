15c20f77c60ecbe68f2f778dbbe7a42a314a25b3
Arindam roy <arindam_ug_23@ei.nits.ac.in>
2025-07-18 22:06:40 +0530

    Add changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set the base path for the project.
+  // This should be the name of your GitHub repository.
+  base: "/React-lottery-ticket/",
   plugins: [react()],
 })
